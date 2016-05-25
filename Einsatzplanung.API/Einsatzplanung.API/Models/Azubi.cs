9fcc0ea371224538278e3ecc8d7d2da36c4e5dc3
oliverzernikow <zernikowo@gmail.com>
2016-05-25 16:28:14 +0200

    Add Azubi and so on

---
+++ Einsatzplanung.API/Einsatzplanung.API/Models/Azubi.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Einsatzplanung.API.Models
+{
+    [Table("azubis")]
+    public class Azubi
+    {
+        [Key]
+        [Column("azubiID")]
+        public int AzubiID { get; set; }
+
+        [Column("vorname")]
+        public string Vorname { get; set; }
+
+        [Column("nachname")]
+        public string Nachname { get; set; }
+
+        [Column("berufID")]
+        public int BerufID { get; set; }
+
+        [Column("ausbilderID")]
+        public int AusbilderID { get; set; }
+
+        [Column("heimatabteilungID")]
+        public int HeimatabteilungID { get; set; }
+
+
+    }
+}
---
+++ Einsatzplanung.API/Einsatzplanung.API/Models/EinsatzplanungContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace Einsatzplanung.API.Models
+{
+    public class EinsatzplanungContext : DbContext
+    {
+        public EinsatzplanungContext() : base(nameOrConnectionString: "einsatzplanungConStr")
+        {
+        }
+
+        public DbSet<Azubi> Azubis { get; set; }
+
+        public DbSet<Ausbilder> Ausbilder { get; set; }
+
+        public DbSet<Fachausbilder> Fachausbilder { get; set; }
+
+    }
+}
