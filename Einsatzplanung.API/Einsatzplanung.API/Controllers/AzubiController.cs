db680d4bf2fb7c11f926cc466bdea654bd21c133
oliverzernikow <zernikowo@gmail.com>
2016-05-25 16:21:32 +0200

    Controller und WebConfig

--- Einsatzplanung.API/Einsatzplanung.API/Controllers/AzubiController.cs
+++ Einsatzplanung.API/Einsatzplanung.API/Controllers/AzubiController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Einsatzplanung.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 
@@ -8,6 +10,40 @@ namespace Einsatzplanung.API.Controllers
 {
     public class AzubiController : ApiController
     {
+        /*
+        * GET /api/azubi/{azubiID}
+        * GET /api/azubis/{ausbilderID}
+        * GET /api/abteilung/{abteilungsID}/azubis
+        *
+        * POST /api/azubi
+        */
 
+        [HttpGet]
+        [Route("api/azubi/{azubiID}")]
+        public Azubi GetAzubi([FromUri] int azubiID)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("api/azubis/{ausbilderID}")]
+        public List<Azubi> GetAzubis([FromUri] int ausbilderID)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("api/abteilung/{abteilungsID}/azubis")]
+        public List<Azubi> GetAbteilungsAzubis([FromUri] int abteilungsID)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [Route("api/azubi")]
+        public HttpResponseMessage PostAzubi([FromBody] Azubi newAzubi)
+        {
+            return null;
+        }
     }
 }
