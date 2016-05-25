32cbc25eba3723112f632dee29a15a38e01fcdbe
oliverzernikow <zernikowo@gmail.com>
2016-05-25 16:26:12 +0200

    azubi controller

--- Einsatzplanung.API/Einsatzplanung.API/Controllers/AzubiController.cs
+++ Einsatzplanung.API/Einsatzplanung.API/Controllers/AzubiController.cs
@@ -1,9 +1,6 @@
 ﻿using Einsatzplanung.API.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Web;
 using System.Web.Http;
 
 namespace Einsatzplanung.API.Controllers
