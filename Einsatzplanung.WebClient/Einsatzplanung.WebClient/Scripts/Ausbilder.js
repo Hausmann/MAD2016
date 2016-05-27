a31072d969c7fdaa957cb0504bc4a46c72dbc2e9
fabiankress <fabi_kress@online.de>
2016-05-27 15:33:16 +0200

    Layout optimiert; Ausbilder.js hinzugefügt

---
+++ Einsatzplanung.WebClient/Einsatzplanung.WebClient/Scripts/Ausbilder.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+    var base = "http://localhost:2222/api/";
+
+    function getAusbilder() {
+        $.getJSON(uri)
+          .done(function (data) {
+              // On success, 'data' contains a list of products.
+              $.each(data, function (key, item) {
+                  // Add a list item for the product.
+                  $('<li>', { text: formatItem(item) }).appendTo($('#test'));
+              });
+          });
+    }
+
+    function formatItem(item) {
+        return item.Name + ': $' + item.Price;
+    }
+})
