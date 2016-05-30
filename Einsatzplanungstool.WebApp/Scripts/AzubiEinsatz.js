6dec8684e4dfbcd02b0c8a815747c1301192a865
LouisMeglitsch <meglloui@gmx.de>
2016-05-30 10:17:09 +0200

    Azubi EinzelAnsicht hinzugefügt

---
+++ Einsatzplanungstool.WebApp/Scripts/AzubiEinsatz.js
@@ -0,0 +1,31 @@
+﻿rhost = "http://localhost:2222/api/";
+
+function getEinsaetze(id) {
+    $.get(rhost + "azubi/" + id + "/einsaetze", function (data) {
+        alert("success");
+        $("#einsatztabelle").append(einsatzAufbereiten(data));
+
+    }).fail(function () {
+        alert("error");
+        var testEinsatz = new Object();
+        testEinsatz.einsatzID = "2";
+        testEinsatz.AbteilungID = "7";
+        testEinsatz.VonDatum = "2016-07-18";
+        testEinsatz.BisDatum = "2016-09-09";
+        testEinsatz.Status = "genehmigt";
+        $("#azubitabelle").append(einsatzAufbereiten(testEinsatz));
+    });
+
+}
+
+function einsatzAufbereiten(data) {
+    result = "<tr>";
+    result += "<td>" + data.einsatzID + "</td>";
+    result += "<td>" + data.AbteilungID + "</td>";
+    result += "<td>" + data.VonDatum + "</td>";
+    result += "<td>" + data.BisDatum + "</td>";
+    result += "<td>" + data.Status + "</td>";
+    result += "</tr>";
+
+    return result;
+}
