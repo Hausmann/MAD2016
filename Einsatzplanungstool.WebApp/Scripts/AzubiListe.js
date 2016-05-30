f14e5135bb394caa5ba22421af7744f7c4e49322
oliverzernikow <zernikowo@gmail.com>
2016-05-30 11:04:34 +0200

    azubi liste

--- Einsatzplanungstool.WebApp/Scripts/AzubiListe.js
+++ Einsatzplanungstool.WebApp/Scripts/AzubiListe.js
@@ -17,12 +17,31 @@ function getAzubi()
 	
 }
 
+function getAzubis(ausbilderId)
+{
+    $.get(rhost + "ausbilder/" + ausbilderId + "/azubis", function (data) {
+        alert("success");
+        for (var i = 0; i < data.length; i++) {
+            $("#azubitabelle").append(azubiAlsHTMLaufbereiten(data[i]));
+
+        }
+
+    }).fail(function () {
+        alert("error");
+        var testazubi = new Object();
+        testazubi.Nachname = "Mustermann";
+        testazubi.Vorname = "Max";
+        testazubi.AzubiID = 11;
+        $("#azubitabelle").append(azubiAlsHTMLaufbereiten(testazubi));
+    });
+}
+
 function azubiAlsHTMLaufbereiten(data)
 {
 	result = "<tr>";
 	result += "<td>" + data.Vorname + "</td>";
 	result += "<td>" + data.Nachname + "</td>";
-	result += "<td>" + data.AzubiID + "</td></tr>";
+	result += "<td>" + data.persNr + "</td></tr>";
 	
 	return result;
 }
